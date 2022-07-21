d07a120d0193bcb161996af0d590a9adc3710c53
Jon Ferreira <85568739+joncfer@users.noreply.github.com>
2022-07-21 03:43:33 -0300

    Primeiro commit.

---
+++ Assets/program.js
@@ -0,0 +1,63 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let blockBoard = false;
+
+function flipCard() {
+    if(blockBoard) return;
+    if(this === firstCard) return;
+
+    this.classList.add('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;        
+    }
+
+    unflipCards(); 
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    blockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip'); 
+
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, blockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    })
+}) ();
+
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard);
+});
